6638241ea36d32c0f96a5337fde467d760db959f
Nikuha <no76813@gmail.com>
2025-05-10 02:32:27 +0500

    Added by Tchanma

---
+++ telegram_bot_plan.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:10b2faa6951a240706535f89c8d74d74507d6f868b7d08450edac0338b6b436f
+size 1409
---
+++ telegram_bot_plan.spec
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a1660e2f64a95cd6fb38145de179aa5552611bfab771a020f82cce446c3fac9a
+size 720
